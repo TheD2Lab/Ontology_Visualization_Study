c78d5843d49c08e87c1f598666e8b9e8b952db2b
Logan Nieto <48600489+IcedTeaGoblin@users.noreply.github.com>
2025-02-03 16:27:24 -0800

    Tutorial Visual Implementation

---
+++ frontend/assets/json/fruit-circlePack.js
@@ -0,0 +1,16 @@
+const tutorialFruitCircle = {
+    "name": "unionOf",
+    "children": [
+        {
+            "name": "Fruits", "value": 600,
+            "children": [
+                {
+                    "name": "SweetFruits", "value": 300
+                },
+                {
+                    "name": "NonSweetFruits", "value": 300
+                }
+            ]
+        }
+    ]
+}
---
+++ frontend/assets/json/pasta-circlePack.js
@@ -0,0 +1,28 @@
+const tutorialPastaCircle = {
+    "name": "disjointWith",
+    "children": [
+        {
+            "name": "EdibleThing", "value": 900,
+            "children": [
+                {
+                    "name": "Pasta", "value": 300
+                },
+                {
+                    "name": "Meat", "value": 300
+                },
+                {
+                    "name": "Fowl", "value": 300
+                },
+                {
+                    "name": "Seafood", "value": 300
+                },
+                {
+                    "name": "Dessert", "value": 300
+                },
+                {
+                    "name": "Fruit", "value": 300
+                }
+            ]
+        }
+    ]
+}
